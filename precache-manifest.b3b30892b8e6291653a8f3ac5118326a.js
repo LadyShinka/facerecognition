a07698a5e02774563db881cd6769bc7b8523c9c5
Maria-Teodora Pop <54820576+LadyShinka@users.noreply.github.com>
2020-02-18 17:20:05 +0000

    Updates

---
+++ precache-manifest.b3b30892b8e6291653a8f3ac5118326a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c7ea9f322e1d031eecb88df7b6ba5a1",
+    "url": "/facerecognition/index.html"
+  },
+  {
+    "revision": "a4fcbf2ab5af2436f6a1",
+    "url": "/facerecognition/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "9bd38ce461fcc8907a57",
+    "url": "/facerecognition/static/css/main.920bc7ed.chunk.css"
+  },
+  {
+    "revision": "a4fcbf2ab5af2436f6a1",
+    "url": "/facerecognition/static/js/2.76c39382.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/facerecognition/static/js/2.76c39382.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9bd38ce461fcc8907a57",
+    "url": "/facerecognition/static/js/main.8018c92f.chunk.js"
+  },
+  {
+    "revision": "5cb746f45af6a5dccf07",
+    "url": "/facerecognition/static/js/runtime-main.9af0037a.js"
+  },
+  {
+    "revision": "dc016ceaaed2ba607dae1a648a72169f",
+    "url": "/facerecognition/static/media/brain.dc016cea.png"
+  }
+]);
